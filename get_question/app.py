0de6b33cac9ebdf42ecb3736dbea0535564981d3
ngr <grischenko.n@gmail.com>
2019-12-01 00:32:15 +0200

    sandbox

--- get_question/app.py
+++ get_question/app.py
@@ -6,18 +6,20 @@ __version__ = "1.00"
 
 import logging
 
-from sosw import Processor as SoswProcessor
+from components.lambda_api import LambdaApi
 from sosw.app import LambdaGlobals, get_lambda_handler
+from sosw.components.dynamo_db import DynamoDbClient
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-class Processor(SoswProcessor):
+class Processor(LambdaApi):
     """
     The main processor of get_question.
     """
     DEFAULT_CONFIG = {}
+    dynamo_db_client: DynamoDbClient = None
 
 
     def __call__(self, event):
@@ -31,6 +33,67 @@ class Processor(SoswProcessor):
         super().__call__(event)
 
         return result
+
+
+    def get_router(self):
+        return {
+            '/':  {
+                'GET': {
+                    'function': self.get_question,
+                    'required_parameters': ['uid'],
+                    'optional_parameters': ['name'],
+                },
+                'POST': {
+                    'function': self.post_question,
+                    'required_parameters': ['uid'],
+                    'optional_parameters': ['name'],
+                }
+            }
+        }
+
+
+    def get_question(self, **kwargs):
+        return {"Think about the structure": 1}
+
+
+    def post_question(self, **kwargs):
+
+        # Save the current result of submitted question.
+        result = kwargs
+        self.dynamo_db_client.put(result)
+
+        # Return next one if required or return result
+        if self.questions_left():
+            return self.get_question(**kwargs)
+        else:
+            return self.get_results(**kwargs)
+
+
+    def questions_left(self, **kwargs):
+        """
+        Return the number of questions left for this user in current session.
+        :param kwargs:
+        :return:
+        """
+        session = kwargs['session']
+        self.dynamo_db_client.get_by_query({"session": session}, return_count=True)
+
+
+    def get_results(self, **kwargs):
+        session = kwargs['session']
+        results = self.dynamo_db_client.get_by_query({"session": session}, fetch_all_fields=True)
+        return results
+
+
+    def is_authenticated(self, api_token=None, env='production'):
+        """
+        Need to implement with Cognito.
+        LambdaAPI currently supports only the external proprietary auth endpoint.
+
+        Current version for my Mark bypasses auth mechanism.
+        """
+        return True
+
 
 global_vars = LambdaGlobals()
 lambda_handler = get_lambda_handler(Processor, global_vars)
