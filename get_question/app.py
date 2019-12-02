4d13d4ed6416b548593d5bfad9f774e59423aa54
ngr <grischenko.n@gmail.com>
2019-12-02 12:40:06 +0200

    some logic

--- get_question/app.py
+++ get_question/app.py
@@ -1,14 +1,19 @@
 """ Get next question for flash-card.
 """
+import random
+
 
 __author__ = 'Nikolay Grishchenko'
 __version__ = "1.00"
 
 import logging
+import time
+import uuid
 
 from components.lambda_api import LambdaApi
 from sosw.app import LambdaGlobals, get_lambda_handler
 from sosw.components.dynamo_db import DynamoDbClient
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -18,7 +23,24 @@ class Processor(LambdaApi):
     """
     The main processor of get_question.
     """
-    DEFAULT_CONFIG = {}
+    DEFAULT_CONFIG = {
+        'init_clients':          ['DynamoDb'],
+        'supported_operators':   ['-', '+'],
+        'questions_per_session': 20,
+        'dynamo_db_config':      {
+            'row_mapper':      {
+                'session':    'S',
+                'created_at': 'N',
+                'uid':        'N',
+                'question':   'S',
+                'answer':     'N',
+                'correct':    'B',
+            },
+            'required_fields': ['session', 'created_at', 'uid'],
+            'table_name':      'question',
+            'hash_key':        'session'
+        }
+    }
     dynamo_db_client: DynamoDbClient = None
 
 
@@ -37,14 +59,14 @@ class Processor(LambdaApi):
 
     def get_router(self):
         return {
-            '/':  {
-                'GET': {
-                    'function': self.get_question,
+            '/': {
+                'GET':  {
+                    'function':            self.ask_question,
                     'required_parameters': ['uid'],
                     'optional_parameters': ['name'],
                 },
                 'POST': {
-                    'function': self.post_question,
+                    'function':            self.post_answer,
                     'required_parameters': ['uid'],
                     'optional_parameters': ['name'],
                 }
@@ -52,19 +74,59 @@ class Processor(LambdaApi):
         }
 
 
-    def get_question(self, **kwargs):
-        return {"Think about the structure": 1}
+    def start_session(self, **kwargs):
+        """
+        You might want to implement saving a session somewhere in a separate table for example and/or
+        to have a service for session handling. The current version just returns a generated session id.
+        """
+
+        sid = str(uuid.uuid4())
+        return sid
 
 
-    def post_question(self, **kwargs):
+    def ask_question(self, **kwargs):
+        session = kwargs.get('session', self.start_session(**kwargs))
 
-        # Save the current result of submitted question.
-        result = kwargs
-        self.dynamo_db_client.put(result)
+        a, b = self.generate_number(session), self.generate_number(session)
+        result = {
+            'a':          max(a, b),
+            'b':          min(a, b),
+            'operator':   random.choice(self.get_operator(session)),
+            'session':    session,
+            'qid':        str(uuid.uuid4()),  # Generate unique ID of question asked for future use.
+            'created_at': time.time(),
+        }
+
+        # Stringify task for simply GUI.
+        result['question'] = f"{result['a']} {result['operator']} {result['b']}"
+
+        # Save asked question to DB
+        self.dynamo_db_client.create(row=result)
+
+        return result
+
+
+    def post_answer(self, **kwargs):
+        """
+        Saves the result of submitted answer and returrns either next question or results.
+        """
+
+        keys = {
+            'session': kwargs['session'],
+            'qid':     kwargs['qid'],
+        }
+
+        question = self.dynamo_db_client.get_by_query(keys=keys)[0]
+
+        question['answered_at'] = time.time()
+        question['answer'] = kwargs.get('answer')
+        question['correct'] = kwargs.get('answer') == eval(question['question'])
+
+        self.dynamo_db_client.put(row=question)
 
         # Return next one if required or return result
-        if self.questions_left():
-            return self.get_question(**kwargs)
+        if self.questions_left(session=kwargs['session']):
+            return self.ask_question(**kwargs)
         else:
             return self.get_results(**kwargs)
 
@@ -76,7 +138,8 @@ class Processor(LambdaApi):
         :return:
         """
         session = kwargs['session']
-        self.dynamo_db_client.get_by_query({"session": session}, return_count=True)
+        return self.config['questions_per_session'] - \
+               self.dynamo_db_client.get_by_query({"session": session}, return_count=True)
 
 
     def get_results(self, **kwargs):
@@ -95,6 +158,17 @@ class Processor(LambdaApi):
         return True
 
 
+    def generate_number(self, session):
+        """ Supposed to get complexity based on user skills identified by ``session`` and generate a number. """
+        return random.randint(0, 20)
+        raise NotImplementedError()
+
+
+    def get_operator(self, session):
+        """ Supposed to get complexity based on user skills identified by ``session`` and return an operator. """
+        return random.choice(self.config['supported_operators'])
+        raise NotImplementedError()
+
+
 global_vars = LambdaGlobals()
 lambda_handler = get_lambda_handler(Processor, global_vars)
-
--- get_question/test/test_main.py
+++ get_question/test/test_main.py
@@ -4,10 +4,11 @@ import boto3
 import datetime
 import logging
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 import os
 
 from collections import defaultdict
+
 
 os.environ["STAGE"] = "test"
 os.environ["autotest"] = "True"
@@ -19,6 +20,13 @@ logging.getLogger('botocore').setLevel(logging.WARNING)
 
 
 class get_question_TestCase(unittest.TestCase):
+    TEST_CONFIG = {
+        'dynamo_db_config': {
+            'table_name': 'autotest_sale_log'
+        },
+    }
+
+    SAMPLE_ANSWER_CORRECT = {'question': '2 + 2', 'answer': '4', 'session': 'foo'}
 
     def setUp(self):
         """
@@ -29,10 +37,11 @@ class get_question_TestCase(unittest.TestCase):
         self.get_config_mock = self.get_config_patcher.start()
         self.get_config_mock.return_value = {}
 
-        CONFIG = {
-            'custom': 1,
-        }
-        self.processor = Processor(test=True, custom_config=CONFIG)
+        self.boto3_patch = patch("boto3.client")
+        self.boto3_mock = self.boto3_patch.start()
+
+        self.processor = Processor(test=True, custom_config=self.TEST_CONFIG)
+        self.processor.dynamo_db_client = MagicMock()
 
 
     def tearDown(self):
@@ -54,3 +63,41 @@ class get_question_TestCase(unittest.TestCase):
         Sample test.
         """
         self.assertTrue(True)
+
+
+    def test_post_answer__validate_result(self):
+        TESTS = {
+            '42 - 42': 0,
+            '42 + 42': 84,
+            '42 - 2':  40,
+        }
+
+        for question, answer in TESTS.items():
+            self.processor.post_answer(**{'question': question, 'answer': answer, 'session': 'foo'})
+            args, kwargs = self.processor.dynamo_db_client.create.call_args
+            self.assertTrue(kwargs['row']['correct'])
+
+
+    def test_post_answer__validate_result__incorrect(self):
+        TESTS = {
+            '42 - 42': 1,
+            '42 + 42': 1,
+            '42 - 2':  1,
+        }
+
+        for question, answer in TESTS.items():
+            self.processor.post_answer(**{'question': question, 'answer': answer, 'session': 'foo'})
+            args, kwargs = self.processor.dynamo_db_client.create.call_args
+            self.assertFalse(kwargs['row']['correct'])
+
+
+    def test_post_answer__continues_questions_left(self):
+        self.processor.questions_left = MagicMock(return_value=5)
+
+        result = self.processor.post_answer(**self.SAMPLE_ANSWER_CORRECT)
+        self.processor.questions_left.assert_called_once()
+
+        # Expecting regular get_question response returned from post_answer
+        EXPECTED = ['question', 'qid', 'session', 'a', 'b', 'operator', 'created_at']
+        for key in EXPECTED:
+            self.assertIsNotNone(result.get(key), f"Missing {key} in result")
